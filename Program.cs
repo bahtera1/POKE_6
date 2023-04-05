39bdfc137289876f46e45e2fbe6f162ace1188fe
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-04-05 09:26:27 +0700

    buat class program

--- Program.cs
+++ Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace POKE_6
 {
-    internal class Program
+    class program
     {
         static void Main(string[] args)
         {
+            program pr=new program();
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukan User ID");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukan Password");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukan Database Tujuan:");
+                    string db = Console.ReadLine();
+                    Console.Write("\n Ketik K untuk tergubung ke database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                }
+            }
         }
+
     }
+
+
 }
  
