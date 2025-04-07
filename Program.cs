e956410d58a6a4b18886d061ba6c98034093ab48
Naufalas <naufalaja080@gmail.com>
2025-04-07 18:57:36 +0700

    w

--- Program.cs
+++ Program.cs
@@ -112,7 +112,7 @@ namespace POKE_6
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Tidak sDapat ssmengakses Database Menggunakan User Tersebut\n");
-                    Console.WriteLine("Tidak Dapa3t mengakses Dratabase Menggunakan User Tersebut\n");
+                    Console.WriteLine("Tidak Dapa3t mengaksesa Dratabase Menggunakan User Tersebut\n");
                     Console.ResetColor();
                 }
             }
