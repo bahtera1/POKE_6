909dd9f19d0a09f78d72e2d01382fa7ab112d2eb
Naufalas <naufalaja080@gmail.com>
2025-04-07 19:02:14 +0700

    33

--- Program.cs
+++ Program.cs
@@ -111,7 +111,7 @@ namespace POKE_6
                 {
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Tidak sDsssapat errngasskssasaea saDatxxsasaabase Menggunakan User Tersebut\n");
+
                     Console.WriteLine("Tidak sDapasaa3tassa  Drata Tersebut\n");
                     Console.ResetColor();
                 }
