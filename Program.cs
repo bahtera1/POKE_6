f8ef2a071cd37b59f92dca2d7e2fea0f87ae93cd
Naufalas <naufalaja080@gmail.com>
2025-04-07 19:01:10 +0700

    dsds

--- Program.cs
+++ Program.cs
@@ -111,7 +111,7 @@ namespace POKE_6
                 {
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Tidak sDsssapat engakssasaea saDatxxsasaabase Menggunakan User Tersebut\n");
+                    Console.WriteLine("Tidak sDsssapat errngakssasaea saDatxxsasaabase Menggunakan User Tersebut\n");
                     Console.WriteLine("Tidak sDapasaa3t ddmengakssssssesasssa Drata Tersebut\n");
                     Console.ResetColor();
                 }
